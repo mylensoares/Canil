0d7d1bb0f58a016bc5e79a6e507f326dc31ba028
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-10-07 16:31:50 -0300

    change class of teste

--- src/Run/Teste.java
+++ src/Run/Teste.java
@@ -1,10 +1,17 @@
 package Run;
 
+import java.util.ArrayList;
+
+import Animais.Cachorro;
+import Animais.Gato;
 import Local.Canil;
 
 public class Teste {
     public static void main(String[] args) {
+        ArrayList<Gato> gatos = new ArrayList<>();
+        ArrayList<Cachorro> cachorros = new ArrayList<>();
+        // tem que cria a lista de gatoas e cachorros com add
         Canil canil = new Canil();
-        canil.atendimento();
+        canil.Iniciarlizar(gatos, cachorros);
     }
 }
