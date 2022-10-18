078ba5b2bad0155ad1a3fe80f9d0d9edd6815fe4
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-10-18 08:05:45 -0300

    populating list of cats

--- src/Run/Teste.java
+++ src/Run/Teste.java
@@ -10,7 +10,19 @@ public class Teste {
     public static void main(String[] args) {
         ArrayList<Gato> gatos = new ArrayList<>();
         ArrayList<Cachorro> cachorros = new ArrayList<>();
-        // tem que cria a lista de gatoas e cachorros com add
+        // tem que cria a lista de gatos e cachorros com add
+        gatos.add(new Gato("Persa", 2022, "Médio", "Preto", false, "Geraldo"));
+        gatos.add(new Gato("Siamês", 2020, "Grande", "Branco", false, "Luiz"));
+        gatos.add(new Gato("Maine Coon", 2018, "Pequeno", "Laranja", true, "Garfield"));
+        gatos.add(new Gato("Angorá", 2020, "Pequeno", "Rajado", false, "Malhado"));
+        gatos.add(new Gato("Ragdoll", 2021, "Grande", "Cinza", false, "Simpson"));
+
+        cachorros.add(new Cachorro("Poodle", 2022, "Médio", "Preto", false, "Rick"));
+        cachorros.add(new Cachorro("Pincher", 2017, "Pequeno", "Marrom", false, "Jaspy"));
+        cachorros.add(new Cachorro("Akita", 2020, "Grande", "Branco", false, "Kira"));
+        cachorros.add(new Cachorro("Beagle", 2019, "Médio", "Branco", false, "Kripto"));
+        cachorros.add(new Cachorro("Chow Chow", 2021, "Grande", "Marrom", false, "Sasha"));
+
         Canil canil = new Canil();
         canil.Iniciarlizar(gatos, cachorros);
     }
